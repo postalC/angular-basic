ddcc80882283ea06008571d4e55993ab35706844
PostalC <chai.wei.tah@gmail.com>
2016-01-17 00:55:45 +0800

    w2-A2-T2

--- work/conFusion/gulpfile.js
+++ work/conFusion/gulpfile.js
@@ -12,19 +12,22 @@ var gulp = require('gulp'),
   changed = require('gulp-changed'),
   rev = require('gulp-rev'),
   browserSync = require('browser-sync'),
+  ngannotate = require('gulp-ng-annotate'),
   del = require('del');
 
+// JS Hints
 gulp.task('jshint', function() {
   return gulp.src('app/scripts/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
 });
 
+//  Use Min
 gulp.task('usemin', ['jshint'], function() {
   return gulp.src('./app/menu.html')
     .pipe(usemin({
       css: [minifycss(), rev()],
-      js: [uglify(), rev()]
+      js: [ngannotate(), uglify(), rev()]
     }).on('error', function(e) {
       console.log(e);
     }))
